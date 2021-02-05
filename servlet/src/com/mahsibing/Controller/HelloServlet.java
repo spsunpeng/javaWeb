879d10c9c0ece2e25c3496a3f4ce56d911df3233
company <476567162@qq.com>
2021-02-05 15:29:11 +0800

    init

---
+++ servlet/src/com/mahsibing/Controller/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.mahsibing.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author sunpeng
+ * @Date 2021-02-05 13:43
+ */
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        req.getSession().setAttribute("username", username);
+        //转发
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
